c8be482bd481ea3caeb9c84d9f296afa9984042c
zengyuxiu <zengyuxiu@nuaa.edu.cn>
2021-04-12 17:06:27 +0800

    set host address

--- config.go
+++ config.go
@@ -3,7 +3,6 @@ package main
 type Config struct {
 	Server struct {
 		Port string `yaml:"port"`
-		Host string `yaml:"host"`
 		Cert string `yaml:"cert"`
 		Key  string `yaml:"key"`
 	} `yaml:"server"`
